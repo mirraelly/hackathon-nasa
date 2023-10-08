ad16ca2293f429d7814f657f57cffacac08b15e0
Mirraely <mirraelymoura@gmail.com>
2023-10-08 19:38:27 -0300

    Update home

--- js/home-animation.js
+++ js/home-animation.js
@@ -1,5 +1,13 @@
 var typed = new Typed('#title-animation', {
-    strings: ['Exoplanet Draco C'],
+    strings: ['Exoplanet Draco c'],
     loop: true,
     typeSpeed: 100,
+});
+
+$(function () {
+    $('.container').starfield({
+        starDensity: 1.0,
+        mouseScale: 0.1,
+        seedMovement: false
+    });
 });
